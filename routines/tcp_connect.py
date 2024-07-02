ddb4fa10e9b476a00a5e0b71a4ce4c0d8d1755a0
Henso ABREU <habreu@lyoserv1.in2p3.fr>
2024-07-02 15:10:25 +0200

    routines to get configuration from picmic0 + modification tcp_connect

--- routines/tcp_connect.py
+++ routines/tcp_connect.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 
 import socket
 import os
-import time,collections,os
+import time,collections,os,platform, pathlib
 import argparse
 import sys
 
 # Fonction pour etablir une connexion TCP avec un serveur.
-def connect_to_server(server, port):
+def connect_to_server(server, port, cfile, outdir):
     try:
         # Tentative de connexion au serveur specifie par l adresse et le port.
         print(f"Tentative de connexion au serveur {server}:{port}")
@@ -16,29 +16,24 @@ def connect_to_server(server, port):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             # Connexion effective au serveur
             client.connect((server, port))
+            #client.settimeout(1.0)
             print("Connexion etablie avec le serveur.")
             # Initialisation du numero de commande a 4.
-            command_number = 4
+            command_number = 3
 
             # Fonction interne pour verifier si la reponse du serveur correspond a l'accuse de reception attendu.
             def check_acknowledgement(command_desc, expected_ack, data):
                 # Decodage de la reponse du serveur et affichage pour debogage.
                 actual_data = data.decode('utf-8').strip()
-                print(f"DEBUG: Reponse reçue pour {command_desc}: '{actual_data}'")
+                print(f"DEBUG: Response received for {command_desc}: '{actual_data}'")
                 # Verification que la reponse contient l'accuse de reception attendu.
                 if expected_ack not in actual_data:
-                    raise Exception(f"Accuse de reception attendu '{expected_ack}' non reçu.")
+                    raise Exception(f"Acknowledgment expected '{expected_ack}' NOT RECEIVED.")
 
             # Liste des commandes a envoyer au serveur avec leur description et l'accuse attendu.
             commands = [
-                (b"#1 ENABLE_DATA_TX_TO_TCP_CLIENT\n", "ENABLE_DATA_TX_TO_TCP_CLIENT", "#1 ENABLE_DATA_TX_TO_TCP_CLIENT #EXECUTED OK"),
-                (b"#2 LOAD_SETUP -FROM .\\Setup\\Setup_K_24_04.dat \n", "Chargement de la configuration", "#2 LOAD_SETUP #EXECUTED OK"),
-                ##(b"#2 LOAD_SETUP -FROM k:\\config\\Setup_06Jun24_V3514.dat\n", "LOAD_SETUP", "#2 LOAD_SETUP #EXECUTED OK"),
-                #(b"#2 LOAD_SETUP -FROM .\\Setup\\Setup_06Jun24_V3514.dat \n", "LOAD_SETUP", "#2 LOAD_SETUP #EXECUTED OK"),
-                ##(b"#3 LOAD_PICMIC_CONFIG_FILE -FROM k:\\config\\myTest.txt\n", "LOAD_PICMIC_CONFIG_FILE", "#3 LOAD_PICMIC_CONFIG_FILE #EXECUTED OK"),
-                (b"#3 LOAD_PICMIC_CONFIG_FILE -FROM .\\PICMIC_ConfigFiles\\picmic_cfg_all_columns_row3.txt \n", "Chargement du fichier de configuration PICMIC", "#3 LOAD_PICMIC_CONFIG_FILE #EXECUTED OK"),
-                #(b"#3 LOAD_PICMIC_CONFIG_FILE -FROM k:\\config\\picmic_cfg_all_columns_row3.txt\n", "LOAD_PICMIC_CONFIG_FILE", "#3 LOAD_PICMIC_CONFIG_FILE #EXECUTED OK"),
-                #(b"#4 LOAD_PICMIC_CONFIG_FILE -FROM k:\\config\\myTest.txt\n", "LOAD_PICMIC_CONFIG_FILE", "#4 LOAD_PICMIC_CONFIG_FILE #EXECUTED OK"),
+                (b"#1 LOAD_SETUP -FROM .\\Setup\\Setup_26Jun24_V3520.dat\n", "LOAD_SETUP", "#1 LOAD_SETUP #EXECUTED OK"),
+                (b"#2 LOAD_PICMIC_CONFIG_FILE -FROM .\\PICMIC_ConfigFiles\\picmic_cfg_all_columns_row3.txt \n", "Chargement du fichier de configuration PICMIC", "#2 LOAD_PICMIC_CONFIG_FILE #EXECUTED OK"),
             ]
 
             # Envoi de chaque commande et attente/controle de l'accuse de reception.
@@ -47,42 +42,68 @@ def connect_to_server(server, port):
                 data = client.recv(1024)
                 check_acknowledgement(desc, ack, data)
                 
-
-            print('HELLO 00')
             line3 = 'LOAD_PICMIC_I2C_REG -add 39 -val '
             line4 = 'START_RUN -TIME 1 -SAVETO '
+			      ##line5 = 'STOP_RUN '
             ##directory = "C:\\Users\\Karim\\TCP_IP\\04_25_2024"
-            directory = "K:\\RUNDATA\\TCP_IP"
+            #directory = "K:\\RUNDATA\\TCP_IP27JUn2024"
+            directory = outdir
             ##directory = "/group/picmic/RUNDATA"
             # Creation du repertoire racine une fois.
             os.makedirs(directory, exist_ok=True)
-            print('HELLO 01')
-            client.sendall(b"#{command_number} LOAD_PICMIC_I2C_REG --add 61 --val \n".encode('utf-8'))
+            ##client.sendall(b"#{command_number} LOAD_PICMIC_I2C_REG --add 61 --val \n")
+            ##client.sendall(b"#{command_number} LOAD_PICMIC_I2C_REG --add 61 --val \n".encode('utf-8'))
 
             # Boucle pour la creation des dossiers specifiques a chaque operation et envoi des commandes correspondantes pour une boucle en VrefN. 
-            for VrefN in range(45, 100):
-                folder_name = f"run{VrefN}"
-                folder_path = os.path.join(directory, folder_name)
-                os.makedirs(folder_path, exist_ok=True)
+            for VrefN in range(50, 66):
+                folder_name = f"vrefn{VrefN}_vrefp"
+#                folder_path = os.path.join(directory, folder_name)
+                folder_path = directory +"\\" + folder_name
+                print(f"Repertoire a creer : {folder_path}")
+                if (platform.system()=="Linux") :
+                    linuxDir='/group/picmic'+pathlib.PureWindowsPath(folder_path).as_posix().strip('K:')
+                    os.makedirs(linuxDir, exist_ok=True)
+                    # print(f"chmod -R g+w {linuxDir}")
+                    os.system("chmod -R g+w "+linuxDir)
+                    print(f"Repertoire LINUX cree avec succes : {linuxDir}")
+                else:
+                    os.makedirs(folder_path, exist_ok=True)
                 print(f"Repertoire cree avec succes : {folder_path}")
                 time.sleep(0.1)
 
                 current_command = f"#{command_number} {line3}{VrefN}\n"
                 client.sendall(current_command.encode('utf-8'))
-                data = client.recv(1024)
-                check_acknowledgement(f"{line3}{VrefN}", f"#{command_number} LOAD_PICMIC_I2C_REG #EXECUTED OK", data)
+                try:
+                    timeout=client.gettimeout()
+                    client.settimeout(10.0)
+                    data = client.recv(1024)
+                    client.settimeout(timeout)
+                    check_acknowledgement(f"{line3}{VrefN}", f"#{command_number} LOAD_PICMIC_I2C_REG #EXECUTED OK", data)
+                except socket.timeout:
+                    print('TIMOUTE')
+                     
                 command_number += 1
 
                 current_command = f"#{command_number} {line4}{folder_path}\n"
                 client.sendall(current_command.encode('utf-8'))
                 data = client.recv(1024)
+                print("ICIC0")
+                print(data)
                 check_acknowledgement(f"{line4}{folder_path}", f"#{command_number} START_RUN #EXECUTED OK", data)
-                command_number += 1
-
-                current_command = f"#{command_number} STOP_RUN\n"
-                client.sendall(current_command.encode('utf-8'))
+                #time.sleep(1)
+                #client.sendall(current_command.encode('utf-8'))
                 data = client.recv(1024)
-                skip_next_response = True
+                print("ICI")
+                print(data)
+                check_acknowledgement(f"START_RUN", f"#{command_number} RUN_FINISHED", data)
+                #time.sleep(1.0)
+                #command_number += 1
+
+                #current_command = f"#{command_number} STOP_RUN\n"
+                #client.sendall(current_command.encode('utf-8'))
+                #data = client.recv(1024)
+                #check_acknowledgement(f"STOP_RUN", f"#{command_number} RUN_FINISHED", data)
+                skip_next_response = False
                 command_number += 1
 
                 if skip_next_response:
@@ -96,7 +117,7 @@ def connect_to_server(server, port):
 
             # Boucle pour la gestion des commandes utilisateur en temps reel.
             while True:
-                command = input("Envoyez une commande (tapez 'disconnect' pour sortir) : ")
+                command = input("Envoyez une commande (tapez 'disconnect' ou Return (commande vide) pour sortir) : ")
                 if command.lower() == "disconnect":
                     print("Deconnexion...")
                     break
@@ -115,8 +136,9 @@ def connect_to_server(server, port):
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-host", "--host_ip" ,help="provide the server IP")
-    parser.add_argument("-port", "--port_number" ,help="provide the port number")
+    parser.add_argument("-host", "--host_ip",     type=str, default='134.158.139.27', help="provide the server IP")
+    parser.add_argument("-port", "--port_number", type=int, default=8267 ,help="provide the port number")
+    parser.add_argument("-dir", "--dir_name",     type=str, defaul='K:\\RUNDATA\\TCP_IP5', help="provide out directory to save data")
     ##parser.add_argument("-sweeping","--RL",help="Sweeping Right(1) or Left(0)", action=argparse.BooleanOptionalAction, default=True)
     args = parser.parse_args()
 
@@ -125,23 +147,18 @@ def main():
         print('Script not executed')
         exit()
 
-    ##host = str(args.host_ip).strip()
-    ##host = str('\'')+str(args.host_ip)+str('\'')
-    ##print('print host=',host)
-    ##port = args.port_number
-
-    host = '134.158.139.27'
+    #  host = str(args.host_ip).strip()
+    ## host = str('\'')+str(args.host_ip)+str('\'')
+    ## print('print host=',host)
+    host = args.host_ip
+    #    host = '134.158.139.27'
+    port = args.port_number
+    dirname=args.dir_name
     
+    #port = 8267
     #export PYTHONIOENCODING=UTF-8
-
-    port = 8266
-
     print('host=',host,',port=',port)
-
-    
-
-    connect_to_server(host, port)
-
+    connect_to_server(host, port, dirname)
     print('----- DONE ----------')
     exit()
 
